7b05befdca55479671c78335666f4c0d63785770
fadhlur28 <muhammadfthoriq@gmail.com>
2026-01-15 19:15:56 +0700

    news_remote with documentation

--- lib/data/datasources/news_remote_data_source.dart
+++ lib/data/datasources/news_remote_data_source.dart
@@ -1,15 +1,32 @@
+/// Data source untuk mengambil data berita dari REST API.
+
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:news_reader/core/constants/api_constants.dart';
 import 'package:news_reader/core/exceptions/news_exceptions.dart';
 import 'package:news_reader/data/models/news_model.dart';
 
+/// Class ini bertanggung jawab untuk:
+/// - Mengirim request HTTP ke News API
+/// - Mengambil data berita berdasarkan kategori atau pencarian
+/// - Mengubah response JSON menjadi objek NewsModel
 class NewsRemoteDataSource {
+
+  /// HTTP client yang digunakan untuk melakukan request API
+
   final http.Client client;
+
+  /// Constructor untuk NewsRemoteDataSource
+  /// [client] digunakan untuk melakukan request HTTP
 
   NewsRemoteDataSource({required this.client});
 
-  /// GET Top Headlines by Category
+  /// Mengambil daftar berita berdasarkan kategori tertentu.
+  /// [category] adalah kategori berita yang ingin diambil
+  /// (contoh: business, sports, technology).
+  /// Return berupa `Future<List<NewsModel>>` yang berisi daftar berita.
+  /// Akan melempar [NewsException] jika request gagal atau terjadi error jaringan.
+
   Future<List<NewsModel>> getNewsByCategory(String category) async {
     try {
       final uri = Uri.parse(
@@ -41,7 +58,11 @@ class NewsRemoteDataSource {
     }
   }
 
-  /// SEARCH NEWS (Everything endpoint)
+  /// Melakukan pencarian berita berdasarkan kata kunci.
+  /// [query] adalah kata kunci pencarian berita.
+  /// Return berupa `Future<List<NewsModel>>` hasil pencarian berita.
+  /// Akan melempar [NewsException] jika proses pencarian gagal.
+
   Future<List<NewsModel>> searchNews(String query) async {
     try {
       final uri = Uri.parse(
